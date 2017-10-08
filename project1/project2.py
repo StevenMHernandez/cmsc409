a88ac32c321ba82eedd9e86f74bc42d6a9c3372a
StevenMHernandez <contact@stevenhdesigns.com>
2017-10-08 17:24:54 -0400

    project2: save values of graphs to a txt file for later analysis (and later reporting), add report

--- project1/project2.py
+++ project1/project2.py
@@ -71,7 +71,7 @@ def calculate_weight_after_delta_d(current_weight, current_pattern, hard_activat
         output = 1 if net > 0 else 0
         delta_d = alpha * (current_pattern[2] - output)
     else:
-        output = (np.tanh(net * k) + 1) / 2 # needs to be unipolar
+        output = (np.tanh(net * k) + 1) / 2
         delta_d = alpha * (current_pattern[2] - output)
 
     current_pattern[0] *= delta_d
@@ -88,58 +88,38 @@ def calculate_weight_after_delta_d(current_weight, current_pattern, hard_activat
 errors = []
 total_errors = []
 weights = [[], [], []]
-soft_outputs = []
 
 
-def render(plt, hard=True, sampleFraction=0.25):
-    df = pd.read_csv(dataFileName, header=None)
+def calculate_error(data_frame, sep_line):
+    error_matrix = project1.get_confusion_matrix(data_frame, sep_line)
+    return 1 - ((error_matrix[1] + error_matrix[0]) / (data_frame[0].count()))
 
-    folder_destination = "images/project2/"
-    folder_destination += "hard/" if hard else "soft/"
-    folder_destination += str(int(100 * sampleFraction)) + "_"
 
-    df = normalize_data_frame(df)
-
-    # smaller amount of random items
-    test_df = df.sample(frac=sampleFraction)
-
-    plt.figure(1)
-    plt = build_height_weight_plot(df)
-    plt.figure(2)
-    plt = build_height_weight_plot(df)
-
-    rand_x = 0.1
-    sep_line = [random.uniform(-rand_x, rand_x), random.uniform(-rand_x, rand_x), random.uniform(-rand_x, rand_x)]
-    original_sep_line = sep_line
+def learn(train_df, test_df, sep_line, number_of_iterations, hard):
     final_sep_line = None
 
-    n = ni
+    for i in range(0, number_of_iterations):
+        print("iteration", i)
 
-    plt.figure(1)
-    # Repeat this `n` times
-    for i in range(0, n):
-        print(i)
-        # if epsilon > err2:
-        #     break
+        total_error = calculate_error(test_df, sep_line)
+        total_errors.append(total_error)
 
-        errorMatrix2 = project1.get_confusion_matrix(df, sep_line)
-        err2 = 1 - ((errorMatrix2[1] + errorMatrix2[0]) / (df[0].count()))
-        total_errors.append(err2)
+        plot_xy_sep_line(sep_line, test_df, color=str(i / number_of_iterations))
 
-        plot_xy_sep_line(sep_line, df, color=str(i / n))
-
-        errorMatrix = project1.get_confusion_matrix(test_df, sep_line)
-        err = 1 - ((errorMatrix[1] + errorMatrix[0]) / (df[0].count()))
+        err = calculate_error(train_df, sep_line)
         errors.append(err)
 
         weights[0].append(sep_line[0])
         weights[1].append(sep_line[1])
         weights[2].append(sep_line[2])
 
+        if epsilon > total_error:
+            break
+
         # mix up the test data frame so that we learn in different ways(?)
-        test_df = test_df.sample(frac=1)
-        # For each element in the data_frame `test_df`
-        for index, row in test_df.iterrows():
+        train_df = train_df.sample(frac=1)
+        # For each element in the data_frame `train_df`
+        for index, row in train_df.iterrows():
             new_weights = calculate_weight_after_delta_d(sep_line, row, hard_activation=hard)
 
             sep_line[0] = new_weights[0]
@@ -148,25 +128,22 @@ def render(plt, hard=True, sampleFraction=0.25):
 
         final_sep_line = sep_line
 
-    plt.figure(1)
-    build_height_weight_plot(df)
-    plot_xy_sep_line(original_sep_line, df, color="g")
-    plot_xy_sep_line(sep_line, df, color="b")
-    print(final_sep_line)
-    print(sep_line[0], original_sep_line[0])
+    return final_sep_line
 
+
+def build_graphs(full_data_frame, folder_destination, original_sep_line, final_sep_line):
     plt.figure(2)
-    build_height_weight_plot(df)
-    plot_xy_sep_line(original_sep_line, df, color="g")
-    plot_xy_sep_line(sep_line, df, color="b")
+    build_height_weight_plot(full_data_frame)
+    plot_xy_sep_line(original_sep_line, full_data_frame, color="g")
+    plot_xy_sep_line(final_sep_line, full_data_frame, color="b")
 
     plt.figure(1)
     plt.axis((0, 1, 0, 1))
-    plt.savefig(folder_destination + "p2_all_sep_lines")
+    plt.savefig(folder_destination + "all_sep_lines")
     plt.gcf().clear()
     plt.figure(2)
     plt.axis((0, 1, 0, 1))
-    plt.savefig(folder_destination + "p2_start_end_lines")
+    plt.savefig(folder_destination + "start_end_lines")
     plt.gcf().clear()
 
     plt.figure(3)
@@ -175,7 +152,7 @@ def render(plt, hard=True, sampleFraction=0.25):
     plt.title("% error over iteration")
     plt.xlabel("Iteration number")
     plt.ylabel("% Error")
-    plt.savefig(folder_destination + "p2_error")
+    plt.savefig(folder_destination + "error")
     plt.gcf().clear()
 
     plt.figure(4)
@@ -185,18 +162,82 @@ def render(plt, hard=True, sampleFraction=0.25):
     plt.title("Weights over iterations")
     plt.xlabel("Iteration number")
     plt.ylabel("Weights")
-    plt.savefig(folder_destination + "p2_weights")
+    plt.savefig(folder_destination + "weights")
     plt.gcf().clear()
 
-    # reset global values
+
+def build_report_file(filename, arr):
+    file = open(filename, 'w')
+
+    for item in arr:
+        file.write("%s\n" % item)
+
+
+def build_report_file_from_weights_arrays(filename, arr):
+    file = open(filename, 'w')
+
+    for i in range(0, len(arr[0])):
+        file.write("%s,%s,%s\n" % (arr[0][i], arr[1][i], arr[2][i]))
+
+
+def build_all_report_files(filepath):
+    build_report_file(filepath + "errors.txt", errors)
+    build_report_file(filepath + "total_errors.txt", total_errors)
+    build_report_file_from_weights_arrays(filepath + "weights.txt", weights)
+
+
+def reset_global_values():
     errors.clear()
+    total_errors.clear()
     weights[0].clear()
     weights[1].clear()
     weights[2].clear()
-    soft_outputs.clear()
     for i in range(1, 5):
         plt.figure(i)
         plt.gcf().clear()
+
+
+def render(plt, hard=True, sampleFraction=0.25):
+    df = pd.read_csv(dataFileName, header=None)
+
+    df = normalize_data_frame(df)
+
+    # smaller amount of random items
+    train_df = df.sample(frac=sampleFraction)
+
+    test_df = df[~df.isin(train_df)]
+
+    plt.figure(1)
+    plt = build_height_weight_plot(df)
+    plt.figure(2)
+    plt = build_height_weight_plot(df)
+
+    rand_x = 0.1
+    sep_line = [random.uniform(-rand_x, rand_x), random.uniform(-rand_x, rand_x), random.uniform(-rand_x, rand_x)]
+    original_sep_line = sep_line
+
+    plt.figure(1)
+    final_sep_line = learn(train_df, test_df, sep_line, ni, hard)
+
+    plt.figure(1)
+    build_height_weight_plot(df)
+    plot_xy_sep_line(original_sep_line, df, color="g")
+    plot_xy_sep_line(sep_line, df, color="b")
+
+    # Store graph images
+    folder_destination = "images/project2/"
+    folder_destination += "hard/" if hard else "soft/"
+    folder_destination += str(int(100 * sampleFraction)) + "_"
+    build_graphs(df, folder_destination, original_sep_line, final_sep_line)
+
+    # Save graphed values to a reports file so that we don't lose data
+    folder_destination = "data/project2/"
+    folder_destination += "hard/" if hard else "soft/"
+    folder_destination += str(int(100 * sampleFraction)) + "_"
+    build_all_report_files(folder_destination)
+
+    # reset global values
+    reset_global_values()
 
 
 def build_report():
@@ -212,12 +253,12 @@ def build_report():
             project1.save_markdown_report(file, [
                 md.h3(activation_type + " activation with a sample size of " + sample_size + "%"),
                 md.images([
-                    ["./images/project2/" + activation_type + "/" + sample_size + "_p2_start_end_lines.png", ""],
-                    ["./images/project2/" + activation_type + "/" + sample_size + "_p2_all_sep_lines.png", ""],
+                    ["./images/project2/" + activation_type + "/" + sample_size + "_start_end_lines.png", ""],
+                    ["./images/project2/" + activation_type + "/" + sample_size + "_all_sep_lines.png", ""],
                 ]),
                 md.images([
-                    ["./images/project2/" + activation_type + "/" + sample_size + "_p2_error.png", "errors"],
-                    ["./images/project2/" + activation_type + "/" + sample_size + "_p2_weights.png", "weights"],
+                    ["./images/project2/" + activation_type + "/" + sample_size + "_error.png", "errors"],
+                    ["./images/project2/" + activation_type + "/" + sample_size + "_weights.png", "weights"],
                 ]),
             ])
 
---
+++ project1/report2.md
@@ -0,0 +1,42 @@
+# Project 2 Report
+
+## CMSC 409 - Artificial Intelligence
+
+## Steven Hernandez
+
+### hard activation with a sample size of 25%
+
+\ ![](./images/project2/hard/25_start_end_lines.png){#id .class width=300 height=200} \ ![](./images/project2/hard/25_all_sep_lines.png){#id .class width=300 height=200} 
+
+\ ![errors](./images/project2/hard/25_error.png){#id .class width=300 height=200} \ ![weights](./images/project2/hard/25_weights.png){#id .class width=300 height=200} 
+
+### hard activation with a sample size of 50%
+
+\ ![](./images/project2/hard/50_start_end_lines.png){#id .class width=300 height=200} \ ![](./images/project2/hard/50_all_sep_lines.png){#id .class width=300 height=200} 
+
+\ ![errors](./images/project2/hard/50_error.png){#id .class width=300 height=200} \ ![weights](./images/project2/hard/50_weights.png){#id .class width=300 height=200} 
+
+### hard activation with a sample size of 75%
+
+\ ![](./images/project2/hard/75_start_end_lines.png){#id .class width=300 height=200} \ ![](./images/project2/hard/75_all_sep_lines.png){#id .class width=300 height=200} 
+
+\ ![errors](./images/project2/hard/75_error.png){#id .class width=300 height=200} \ ![weights](./images/project2/hard/75_weights.png){#id .class width=300 height=200} 
+
+### soft activation with a sample size of 25%
+
+\ ![](./images/project2/soft/25_start_end_lines.png){#id .class width=300 height=200} \ ![](./images/project2/soft/25_all_sep_lines.png){#id .class width=300 height=200} 
+
+\ ![errors](./images/project2/soft/25_error.png){#id .class width=300 height=200} \ ![weights](./images/project2/soft/25_weights.png){#id .class width=300 height=200} 
+
+### soft activation with a sample size of 50%
+
+\ ![](./images/project2/soft/50_start_end_lines.png){#id .class width=300 height=200} \ ![](./images/project2/soft/50_all_sep_lines.png){#id .class width=300 height=200} 
+
+\ ![errors](./images/project2/soft/50_error.png){#id .class width=300 height=200} \ ![weights](./images/project2/soft/50_weights.png){#id .class width=300 height=200} 
+
+### soft activation with a sample size of 75%
+
+\ ![](./images/project2/soft/75_start_end_lines.png){#id .class width=300 height=200} \ ![](./images/project2/soft/75_all_sep_lines.png){#id .class width=300 height=200} 
+
+\ ![errors](./images/project2/soft/75_error.png){#id .class width=300 height=200} \ ![weights](./images/project2/soft/75_weights.png){#id .class width=300 height=200} 
+
